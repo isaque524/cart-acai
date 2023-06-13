27e53fb5913b92c3e71112a48f4fddf72249340b
isaque524 <isaquesilvadamasceno@gmail.com>
2023-06-13 14:51:22 -0300

    fimCarrinho

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -11,14 +11,18 @@ import { AcaiSorveteService } from 'src/app/services/tipos.service';
 export class CartComponent implements OnInit {
   public products: any = [];
   public grandTotal!: number;
+  public pegarSabores: any = [];
+  public pegarAdicionas: any = [];
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.cartService.getProducts().subscribe((res) => {
       this.products = res;
+      console.log(this.products);
       this.grandTotal = this.cartService.getTotalPrice();
     });
   }
+
   removeItem(item: any) {
     this.cartService.removeCartItem(item);
   }
