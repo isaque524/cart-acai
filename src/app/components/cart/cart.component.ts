f234ee38f84bf5cafe90c70bd064b5d6507c333c
isaque524 <isaquesilvadamasceno@gmail.com>
2023-06-21 15:33:48 -0300

    pdf

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { CartService } from './../../services/cart.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as html2pdf from 'html2pdf.js';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cart',
@@ -10,7 +13,8 @@ export class CartComponent implements OnInit {
   public products: any = [];
   public grandTotal!: number;
   public contarAdicionais: boolean = false;
-  constructor(private cartService: CartService) {}
+  public hidden: boolean = false;
+  constructor(private cartService: CartService, public router: Router) {}
 
   ngOnInit(): void {
     this.cartService.getProducts().subscribe((res) => {
@@ -22,7 +26,32 @@ export class CartComponent implements OnInit {
   removeItem(item: any) {
     this.cartService.removeCartItem(item);
   }
-  emptycart() {
+  LimparCarrinho() {
     this.cartService.removeAllCart();
+  }
+
+  Dowload() {
+    var element = document.getElementById('pedido');
+
+    var opt = {
+      margin: [10, 10, 10, 10],
+      filename: 'pedido.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+    };
+    html2pdf().from(element).set(opt).save();
+    Swal.fire({
+      title: 'Enviado com sucesso?',
+      text: 'Por favor espere o pdf ser gerado para realizar uma nova compra',
+      icon: 'success',
+      confirmButtonColor: '#3085d6',
+      confirmButtonText: 'realizar uma nova compra',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.router.navigate(['/products']);
+        this.cartService.removeAllCart();
+      }
+    });
   }
 }
